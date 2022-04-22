8c9a1541bfb5f45de1acd2f8836719d2b8092c9b
MaryanneNganga <mary.anne.nganga.fetzer@gmail.com>
2022-04-22 14:55:09 +0200

    First commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vue3Material from 'vue3-material';
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(Vue3Material).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <div class="counter">
+      {{$store.state.counter}}
   </div>
+  <div class="buttons">
+    <button @click ="$store.commit('decreaseCounter')">-</button>
+    <button @click="$store.commit('increaseCounter')">+ </button>
+  </div>
+  <vm-container>
+        <vm-button>It works!</vm-button>
+    </vm-container>
+  </div>
+ 
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  
+  
+ 
 }
 </script>
+
+<style>
+div{
+  margin-bottom: 10px;
+}
+.counter{
+  font-size: 80px;
+}
+.buttons button{
+  font-size: 40px;
+  width: 100px;
+  margin: 0 10px;
+}
+</style>
